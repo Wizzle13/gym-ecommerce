75f36557e4f67310a498af8c23d94b83adbf06c3
Vue Lee <vuelee09@gmail.com>
2022-08-01 20:52:19 -0500

    remove content in seed file

--- server/seeders/seeds.js
+++
@@ -1,8 +0,0 @@
-// import the gql tagged template function
-const { gql } = require('apollo-server-express');
-
-// create our typeDefs
-const typeDefs = gql``;
-
-// export the typeDefs
-module.exports = typeDefs;
