24f4c241385902951cde62beccf162004ad0a067
CBurton <burton.chris13@gmail.com>
2022-08-02 20:19:37 -0500

    update auth file

---
+++ server/models/User.js
@@ -0,0 +1,8 @@
+userSchema.pre('save', async function(next) {
+    if (this.isNew || this.isModified('password')) {
+      const saltRounds = 10;
+      this.password = await bcrypt.hash(this.password, saltRounds);
+    }
+  
+    next();
+  });
--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
     typeDefs,
     resolvers,
-    context: authMiddleware
+    // context: authMiddleware
 });
 
 const app = express();
@@ -35,7 +35,7 @@ const startApolloServer = async (typeDefs, resolvers) => {
     if (process.env.NODE_ENV === 'production') {
         app.use(express.static(path.join(__dirname, '../client/build')));
     }
-    app.get('*', (req, res) => {
+    app.get('/', (req, res) => {
         res.sendFile(path.join(__dirname, '../client/build/index.html'));
     });
 
