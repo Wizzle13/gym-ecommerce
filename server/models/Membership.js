028e817402a8666b93cb691f23c4c05c5b1e8ac0
Vue Lee <vuelee09@gmail.com>
2022-08-06 09:17:38 -0500

    add some more resolvers and models

---
+++ server/models/Membership.js
@@ -0,0 +1,8 @@
+const { Schema, model } = require('mongoose');
+
+const membershipSchema = new Schema(
+
+
+)
+
+module.exports = Membership;
---
+++ server/models/Trainer.js
@@ -0,0 +1,8 @@
+const { Schema, model } = require('mongoose');
+
+const trainerSchema = new Schema(
+
+
+)
+
+module.exports = Trainer;
