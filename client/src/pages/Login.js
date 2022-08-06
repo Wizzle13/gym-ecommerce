0e59d5b5d9fbc1bc86da97899c2e7040bb52a1cd
CBurton <burton.chris13@gmail.com>
2022-08-05 20:44:54 -0500

    converted to SPA added login page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -41,8 +41,8 @@ const Login = (props) => {
   return (
     <main className='flex-row justify-center mb-4'>
       <div className='col-12 col-md-6'>
-        <div className='card'>
-          <h4 className='card-header'>Login</h4>
+        <div className='card' id='contact'>
+          <h1 className='card-header'>Login</h1>
           <div className='card-body'>
             {/* <form onSubmit={handleFormSubmit}> */}
             <form>
@@ -64,9 +64,9 @@ const Login = (props) => {
                 // value={formState.password}
                 // onChange={handleChange}
               />
-              <button className='btn d-block w-100' type='submit'>
-                Submit
-              </button>
+              <input type='submit' value='submit' />
+                
+              
             </form>
             {/* {error && <div>Login failed</div>} */}
           </div>
--- client/src/pages/Navbar.jsx
+++ client/src/pages/Navbar.jsx
@@ -1,6 +1,6 @@
 import React,{useState} from 'react'
 import logo from '../images/gymlogo.png';
-import {Link} from 'react-scroll';
+import {Link} from 'react-router-dom';
 export default function Navbar() {
     const[nav,setnav] = useState(false);
     const changeBackground =() =>{
@@ -15,7 +15,7 @@ export default function Navbar() {
 
     return (
         <nav className={nav ?"nav active":"nav"}>
-            <Link to='main' smooth={true} duration={1000} className ='logo'>
+            <Link to='/' smooth={true} duration={1000} className ='logo'>
                 <img src={logo} alt=""/>
             </Link>
             <input className='menu-btn' type='checkbox' id='menu-btn' />
@@ -23,12 +23,12 @@ export default function Navbar() {
                   <span className='nav-icon'></span>
               </label>
               <ul className="menu">
-                  <li><Link to="main" smooth={true} duration={1000}>Home</Link></li>
-                  <li><Link to="features" smooth={true} duration={1000}>What we Offer</Link></li>
-                  <li><Link to="presentaion" smooth={true} duration={1000}>Team</Link></li>
+                  <li><Link to="/" smooth={true} duration={1000}>Home</Link></li>
+                  <li><Link to="weoffer" smooth={true} duration={1000}>What we Offer</Link></li>
+                  <li><Link to="team" smooth={true} duration={1000}>Team</Link></li>
                   <li><Link to="about" smooth={true} duration={1000}>About</Link></li>
                   <li><Link to="contact" smooth={true} duration={1000}>Contact Us</Link></li>
-                  <li><Link to="#" smooth={true} duration={1000}>Sign In </Link></li>
+                  <li><Link to="Login" smooth={true} duration={1000}>Sign In </Link></li>
 
               </ul>
 
